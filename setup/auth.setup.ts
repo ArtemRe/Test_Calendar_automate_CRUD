2b25c37c8e2bcc0452cba94dc7b895a1f124ceaf
Damirel <rechembeiartem@gmail.com.com>
2026-05-05 02:06:42 +0300

    update

--- setup/auth.setup.ts
+++ setup/auth.setup.ts
@@ -9,25 +9,22 @@ chromium.use(stealth());
 const authFile = path.join(__dirname, '../.auth/user.json');
 
 setup('authenticate', async () => {
-  setup.setTimeout(300000); // 5 хвилин на логін
-  
+  setup.setTimeout(300000);
+
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
 
   await page.goto('https://calendar.google.com');
+  console.log(' You have 90 SECONDS. The script will simply wait for this time.');
 
-  console.log('\n======================================================');
-  console.log(' БУДЬ ЛАСКА, ЗАЛОГІНЬТЕСЬ У ВІКНІ БРАУЗЕРА');
-  console.log(' У вас є 90 СЕКУНД. Скрипт просто почекає цей час.');
-  console.log('======================================================\n');
 
-  // Найпростіший варіант: просто чекаємо 90 секунд.
+
   await page.waitForTimeout(90000);
-  
-  // Зберігаємо стан авторизації
+
+
   await context.storageState({ path: authFile });
-  console.log('--- СТАН АВТОРИЗАЦІЇ ЗБЕРЕЖЕНО ---');
+  console.log('AUTHORIZATION STATUS SAVED');
 
   await browser.close();
 });
